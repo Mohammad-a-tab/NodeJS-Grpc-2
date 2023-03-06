225caf35f6badd9606a2878f7fb35a1338d91858
Mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-03-06 22:40:39 +0330

    make changes

--- services/product/product.service.js
+++ services/product/product.service.js
@@ -1,5 +1,23 @@
-require("./config/db.connection")
+const { listProduct, getProduct, createProduct, updateProduct, deleteProduct } = require('./functions/product.grpc');
+require("./config/db.connection");
 const grpc = require("@grpc/grpc-js");
 const protoLoader = require("@grpc/proto-loader");
 const path = require("path");
 const protoPath = path.join(__dirname, "..", "..", "protos/product.proto");
+const productProto = protoLoader.loadSync(protoPath);
+const {ProductPackage} = grpc.loadPackageDefinition(productProto);
+const ProductServiceURL = "localhost:4001";
+function main() {
+    const server = new grpc.Server();
+    server.addService(ProductPackage.ProductService.service, {
+        listProduct,
+        getProduct,
+        createProduct,
+        updateProduct,
+        deleteProduct
+    });
+    server.bindAsync(ProductServiceURL, grpc.ServerCredentials.createInsecure(), (err, port) => {
+        if(err) return console.log("Error :", err);
+        console.log("GRPC Service Running On port" + port);
+    })
+}
