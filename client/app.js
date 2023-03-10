8e1d78a86df9b76990a91295c4dd0e09ff9a3d7a
Mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-03-10 21:26:06 +0330

    make changes

--- client/app.js
+++ client/app.js
@@ -1,9 +1,35 @@
-const express = require("express");
 const { AllRoutes } = require("./routes/index.routes");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const express = require("express");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(AllRoutes);
+app.use( "/api-doc" , swaggerUI.serve , swaggerUI.setup(swaggerJsDoc({
+        swaggerDefinition : {
+            openapi: "3.0.0", 
+            info : {
+                title : "Boto Start Store",
+                version : "2.0.0",
+                description : "بزرگترین مرجع ارائه پکیج برنامه نویسی",
+                contact : {
+                    name : "Mohammad Aali",
+                    url : "http://github.com/Mohammad-a-tab",
+                    email : "mohammad.programmer.tab@gmail.com",
+                },
+                
+            },
+            servers : [
+                {
+                    url : "http://localhost:4000"
+                }
+            ],
+        },
+        apis : ["./routes/**/*.js"]
+    }),
+    {explorer : true},
+));
 app.use((req, res, next) => {
     return res.status(404).json({
         statusCode : 404,
--- services/product/functions/product.grpc.js
+++ services/product/functions/product.grpc.js
@@ -1,4 +1,13 @@
-async function listProduct (call, callback) {}
+const { ProductModel } = require("../model/product.model")
+
+async function listProduct (call, callback) {
+    try {
+        const products = await ProductModel.find({});
+        callback(null, {products})
+    } catch (error) {
+        callback(error, null);
+    }
+}
 async function getProduct (call, callback) {}
 async function createProduct (call, callback) {}
 async function updateProduct (call, callback) {}
