d35562f127ba421aa62ec3263abebdd026a7ee27
Mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-03-11 23:54:34 +0330

    make changes

--- services/product/model/product.model.js
+++ services/product/model/product.model.js
@@ -1,17 +1,18 @@
-const {default : mongoose} = require("mongoose")
+const { default: mongoose, model } = require("mongoose");
+
 const ProductSchema = new mongoose.Schema({
-    id: {type : String},
-    title: {type : String},
-    price: {type : String}
-});
-// ProductSchema.pre("save", function(next) {
-//     const self = this;
-//     self.constructor.count(async function(err, data) {
-//         if(err) return next(err);
-//         self.set({id: (data + 1)})
-//         next();
-//     })
-// })
+    id: {type: Number},
+    title: {type: String},
+    price: {type: String}
+})
+ProductSchema.pre("save", function(next) {
+    const self = this;
+    self.constructor.count(async function(err, data) {
+        if(err) return next(err);
+        self.set({id: (data + 1)})
+        next();
+    })
+})
 module.exports = {
-    ProductModel : mongoose.model("Product", ProductSchema)
+    ProductModel : mongoose.model("product", ProductSchema)
 }
