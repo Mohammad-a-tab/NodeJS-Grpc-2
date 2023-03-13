17b9163b796e299278ba6164eac0354dc713e693
Mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-03-14 00:24:13 +0330

    make changes

--- services/blog/model/blog.model.js
+++ services/blog/model/blog.model.js
@@ -2,5 +2,7 @@ const {default : mongoose} = require("mongoose")
 const BlogModel = new mongoose.Schema({
     id: {type:  Number, unique: true},
     title: {type: String, required: true},
-    description: {type: String}
+    text : {type : String , required : true},
+    image : {type : String, required : true},
+    tags : {type : [String] , default : []},
 })
