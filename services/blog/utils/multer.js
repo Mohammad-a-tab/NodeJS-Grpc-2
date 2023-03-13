f34607a596f667a959c64570abe573934088deb4
Mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-03-14 01:57:17 +0330

    make changes'

--- services/blog/utils/multer.js
+++ services/blog/utils/multer.js
@@ -1,8 +1,9 @@
+const createHttpError = require("http-errors");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 
-function createRouteForBlog(req) {
+function createRouteForBlogs(req) {
     const date = new Date();
     const monthNumber = date.getMonth() + 1;
     const Year = date.getFullYear().toString();
@@ -12,4 +13,35 @@ function createRouteForBlog(req) {
     req.body.fileUploadPath = path.join("uploads" , "blogs" , Year , Month , Day)
     fs.mkdirSync(directory , {recursive : true});
     return directory
+}
+const storageForBlog = multer.diskStorage({
+    destination : (req,file,cb) => {
+        if(file?.originalname){
+            const filePath = createRouteForBlogs(req);
+            return cb(null , filePath)
+        }
+        cb(null, null)
+    },
+    filename : (req,file,cb) => {
+        if(file.originalname){
+            const ext = path.extname(file.originalname);
+            const fileName = String(new Date().getTime() + ext);
+            req.body.filename = fileName;
+            return cb(null, fileName)
+        }
+        cb(null, null)
+    }
+});
+function fileFilter(req, file, cb) {
+    const ext = path.extname(file.originalname);
+    const mimetypes = [".jpg", ".jpeg", ".png", ".webp", ".gif"];
+    if (mimetypes.includes(ext)) {
+      return cb(null, true);
+    }
+    return cb(createHttpError.BadRequest("فرمت ارسال شده تصویر صحیح نمیباشد"));
+}
+const pictureMaxSize = 4 * 1000 * 1000;     //4MB
+const uploadFileBlog = multer({storage : storageForBlog , fileFilter , limits : {fileSize : pictureMaxSize}});
+module.exports = {
+    uploadFileBlog
 }
