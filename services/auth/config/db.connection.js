51afb2468478828923fb638c6552a784a700be5c
Mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-03-18 13:30:01 +0330

    create auth section in this project

---
+++ services/auth/config/db.connection.js
@@ -0,0 +1,4 @@
+const { default: mongoose } = require("mongoose");
+module.exports = mongoose.connect("mongodb://localhost:27017/grpc-nodejs")
+.then(() => console.log("connected to MongoDB"))
+.catch(err => console.log(err))
