0bf64ea037ada917356923b2a56ca47a881ac29b
Mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-03-18 14:15:12 +0330

    make changes

--- services/user/model/user.model.js
+++ services/user/model/user.model.js
@@ -1,6 +1,10 @@
 const { default: mongoose } = require("mongoose")
 const UserSchema = new mongoose.Schema({
-
+    firstName: {type: String, default: ""},
+    lastName: {type: String, default: ""},
+    email: {type: String},
+    phone: {type: String, required: true},
+    password: {type: String, required: true}
 })
 module.exports = {
     UserModel : mongoose.model("user", UserSchema)
