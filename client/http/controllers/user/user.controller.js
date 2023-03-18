b57b31cd6dbf2790179b565af7ec9c3772a89212
Mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-03-18 14:47:58 +0330

    make changes

--- client/http/controllers/user/user.controller.js
+++ client/http/controllers/user/user.controller.js
@@ -1,6 +1,6 @@
 const grpc = require("@grpc/grpc-js");
 const protoLoader = require("@grpc/proto-loader");
-const authProtoPath = path.join(__dirname, "..", "..", "..", "..", "protos", "auth.proto");
+const authProtoPath = path.join(__dirname, "..", "..", "..", "..", "protos", "user.proto");
 const authProto = protoLoader.loadSync(authProtoPath);
 const {AuthPackage}  = grpc.loadPackageDefinition(authProto);
 const AuthServiceURL = "localhost:4003";
@@ -8,7 +8,7 @@ const authClient = new AuthPackage.AuthService(AuthServiceURL, grpc.credentials.
 const path = require("path");
 
 class AuthController {
-
+    
 }
 module.exports = {
     AuthController : new AuthController()
