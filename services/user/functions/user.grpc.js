020a8007c471656586c8dd75c596a2faf9497686
Mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-03-30 14:08:15 +0430

    make changes

--- services/user/functions/user.grpc.js
+++ services/user/functions/user.grpc.js
@@ -19,20 +19,29 @@ async function getListOfUser (call, callback) {
 }
 async function getUser (call, callback) {
     try {
-        
+        const {id} = call.request;
+        const user = await UserModel.findById({_id : id});
+        callback(null, {user})
     } catch (error) {
         callback(error, null)
     }
 }
 async function updateUser (call, callback) {
     try {
-        
+        const {firstName, lastName, email, phone, id} = call.request;
+        await UserModel.findById({_id: id})
+        const result = await UserModel.updateOne({_id: id}, {$set : {firstName, lastName, email, phone}})
+        if(result.modifiedCount > 0) {
+            callback(null, {status: "200", message: "successfully updated"})
+        }
+        callback(null, {status: "400", message: "Unsuccessful"})
     } catch (error) {
         callback(error, null)
     }
 }
 async function deleteUser (call, callback) {
     try {
+        const {id} = call.request;
         
     } catch (error) {
         callback(error, null)
